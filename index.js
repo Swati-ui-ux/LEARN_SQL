c769bcdb72dcd450a709737f330121e8cb53bec3
Swati singh <swatigola274@email.com>
2026-03-23 21:46:16 +0530

    create table

--- index.js
+++ index.js
@@ -19,18 +19,59 @@ connection.connect((err) => {
     console.log("Connected to database");
     
     const creationQuery = `create table Students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(25), email VARCHAR(25))`;
+    const Users = `create table Users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(25), email VARCHAR(25))`;
+    const Buses = `create table Buses (id INT AUTO_INCREMENT PRIMARY KEY, busNumber VARCHAR(20),totalSeats INT,availableSeats VARCHAR(20))`;
+    const Booking = `create table Booking (id INT AUTO_INCREMENT PRIMARY KEY , seatNumber INT) `
     
-    connection.execute(creationQuery, (err) => {
+    const payment = `create table Payment (id INT AUTO_INCREMENT PRIMARY KEY,amountPaid VARCHAR(25),paymentStatus VARCHAR(25))`
+    // connection.execute(payment, (err) => {
+    //     if (err) {
+    //         console.log("Error when creating payment table")
+    //         connection.end()
+    //         return
+    //     }
+    //     console.log("Payment table created successfully")
     
-        if(err){
-            console.log("Error creating table", err);
-            connection.end();
-            return;
-        }
-        console.log("Table created successfully");
-    })
+    // })
+    
+    
 })
 
+
+
+
+
+
+
+
+
+    // connection.execute(Buses, (err) => {
+    
+    //     if (err) {
+    //         console.log("Error when bus created", err)
+    //         connection.end()
+    //         return
+    //     }
+    //     console.log("buses created successfully")
+    
+    // })
+    // connection.execute(creationQuery, (err) => {
+    //     if(err){
+    //         console.log("Error creating table", err);
+    //         connection.end();
+    //         return;
+    //     }
+    //     console.log("Table created successfully");
+    // })
+
+
+    // connection.execute(Users, (err) => {
+    //     if (err) {
+    //     console.log("Error from connection",err)
+    //     }
+    //     connection.end()
+    //     console.log("Data created")
+    // })
 
 
 
